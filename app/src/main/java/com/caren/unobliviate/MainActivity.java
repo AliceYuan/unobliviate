d75f224a100c2491093bcfc5e7288bf36ef73500
calren <calren24@gmail.com>
2018-09-09 09:19:24 -0700

    build in logic to only show answer when question card is tapped

--- app/src/main/java/com/caren/unobliviate/MainActivity.java
+++ app/src/main/java/com/caren/unobliviate/MainActivity.java
@@ -2,12 +2,25 @@ package com.caren.unobliviate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View answerSideView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        answerSideView = findViewById(R.id.flashcard);
+
+        answerSideView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answerSideView.setVisibility(View.GONE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
