132a8dec0ee7916745533a3c0e7ed9e0a2b464a3
Caren Chang <calren24@gmail.com>
2018-09-11 21:23:15 -0700

    Add a button that will toggle hiding and showing the answer choices

--- app/src/main/java/com/caren/unobliviate/MainActivity.java
+++ app/src/main/java/com/caren/unobliviate/MainActivity.java
@@ -1,8 +1,9 @@
 package com.caren.unobliviate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     private View option1View;
     private View option2View;
     private View option3View;
+
+    private ImageView toggleShowingAnswersIcon;
+
+    private boolean isShowingAnswers = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         option1View = findViewById(R.id.option_1);
         option2View = findViewById(R.id.option_2);
         option3View = findViewById(R.id.option_3);
+        toggleShowingAnswersIcon = findViewById(R.id.toggle_choices_visibility);
 
         questionSideView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,6 +83,25 @@ public class MainActivity extends AppCompatActivity {
                         .original_option_answer_color, null));
                 option3View.setBackgroundColor(getResources().getColor(R.color
                         .original_option_answer_color, null));
+            }
+        });
+
+        toggleShowingAnswersIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isShowingAnswers) {
+                    option1View.setVisibility(View.GONE);
+                    option2View.setVisibility(View.GONE);
+                    option3View.setVisibility(View.GONE);
+                    toggleShowingAnswersIcon.setImageResource(R.drawable.show_icon);
+                } else {
+                    option1View.setVisibility(View.VISIBLE);
+                    option2View.setVisibility(View.VISIBLE);
+                    option3View.setVisibility(View.VISIBLE);
+                    toggleShowingAnswersIcon.setImageResource(R.drawable.hide_icon);
+                }
+
+                isShowingAnswers = !isShowingAnswers;
             }
         });
     }
