4d37b7dccb5fbfcb6f9f7da00641319e66d6bd26
Caren Chang <calren24@gmail.com>
2018-09-11 21:04:40 -0700

    add answer choices and indication of whether right answer was selected

--- app/src/main/java/com/caren/unobliviate/MainActivity.java
+++ app/src/main/java/com/caren/unobliviate/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     private View questionSideView;
     private View answerSideView;
 
+    private View option1View;
+    private View option2View;
+    private View option3View;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
         questionSideView = findViewById(R.id.flashcard_question);
         answerSideView = findViewById(R.id.flashcard_answer);
+        option1View = findViewById(R.id.option_1);
+        option2View = findViewById(R.id.option_2);
+        option3View = findViewById(R.id.option_3);
 
         questionSideView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +37,46 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 answerSideView.setVisibility(View.GONE);
                 questionSideView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        option1View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                option1View.setBackgroundColor(getResources().getColor(R.color
+                        .wrong_answer_indication_color, null));
+                option3View.setBackgroundColor(getResources().getColor(R.color
+                        .right_answer_indication_color, null));
+            }
+        });
+
+        option2View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                option2View.setBackgroundColor(getResources().getColor(R.color
+                        .wrong_answer_indication_color, null));
+                option3View.setBackgroundColor(getResources().getColor(R.color
+                        .right_answer_indication_color, null));
+            }
+        });
+
+        option3View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                option3View.setBackgroundColor(getResources().getColor(R.color
+                        .right_answer_indication_color, null));
+            }
+        });
+
+        findViewById(R.id.root).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                option1View.setBackgroundColor(getResources().getColor(R.color
+                        .original_option_answer_color, null));
+                option2View.setBackgroundColor(getResources().getColor(R.color
+                        .original_option_answer_color, null));
+                option3View.setBackgroundColor(getResources().getColor(R.color
+                        .original_option_answer_color, null));
             }
         });
     }
