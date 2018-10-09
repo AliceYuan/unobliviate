b0b7b5c87a02282a10895492840cfd0221392ef4
calren <calren24@gmail.com>
2018-10-08 20:04:50 -0700

    add button that opens new activity

--- app/src/main/java/com/caren/unobliviate/MainActivity.java
+++ app/src/main/java/com/caren/unobliviate/MainActivity.java
@@ -1,5 +1,6 @@
 package com.caren.unobliviate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 questionSideView.setVisibility(View.GONE);
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.addBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivity(myIntent);
             }
         });
     }
