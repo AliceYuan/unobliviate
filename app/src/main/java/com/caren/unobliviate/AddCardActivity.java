f893fa79a8f3153403c6aecd3224b53f51bda008
calren <calren24@gmail.com>
2018-10-08 22:56:32 -0700

    add error msg if not both answer and question is filled in

--- app/src/main/java/com/caren/unobliviate/AddCardActivity.java
+++ app/src/main/java/com/caren/unobliviate/AddCardActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -29,10 +30,21 @@ public class AddCardActivity extends AppCompatActivity {
         findViewById(R.id.saveBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String enteredQuestion = ((EditText) findViewById(R.id.questionTextField)).getText().toString();
+                String enteredAnswer = ((EditText) findViewById(R.id.answerTextField)).getText().toString();
+
+                if (enteredAnswer.isEmpty() || enteredQuestion.isEmpty()) {
+                    Toast.makeText(AddCardActivity.this,
+                            "Must enter both Question and Answer!",
+                            Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
                 Intent data = new Intent();
                 // Pass relevant data back as a result
-                data.putExtra("question", ((EditText) findViewById(R.id.questionTextField)).getText().toString());
-                data.putExtra("answer", ((EditText) findViewById(R.id.answerTextField)).getText().toString());
+                data.putExtra("question", enteredQuestion);
+                data.putExtra("answer", enteredAnswer);
                 setResult(RESULT_OK, data); // set result code and bundle data for response
                 finish(); // closes the activity, pass data to parent
             }
