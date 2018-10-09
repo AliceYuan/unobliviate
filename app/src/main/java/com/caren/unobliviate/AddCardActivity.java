146377eea8637138b24306e9d4eeaaa8b353d2af
calren <calren24@gmail.com>
2018-10-08 20:10:15 -0700

    add cancel btn that exits add card screen

--- app/src/main/java/com/caren/unobliviate/AddCardActivity.java
+++ app/src/main/java/com/caren/unobliviate/AddCardActivity.java
@@ -2,6 +2,7 @@ package com.caren.unobliviate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class AddCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.cancelBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
