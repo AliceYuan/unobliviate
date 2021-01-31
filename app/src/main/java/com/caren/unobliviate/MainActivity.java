adb3be40215fcd811bcd69d1584eed5806feae5d
AliceYuan <me@aliceyuan.ca>
2021-01-31 18:40:36 -0500

    Update to latest gradle and update to androidx for all files

--- app/src/main/java/com/caren/unobliviate/MainActivity.java
+++ app/src/main/java/com/caren/unobliviate/MainActivity.java
@@ -1,11 +1,13 @@
 package com.caren.unobliviate;
 
 import android.content.Intent;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
