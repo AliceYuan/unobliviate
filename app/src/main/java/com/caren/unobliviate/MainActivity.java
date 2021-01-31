dd89e70a89d0d03b98f6f6db7ec52b8e92ce63c8
AliceYuan <me@aliceyuan.ca>
2021-01-31 18:40:36 -0500

    Update to latest gradle and update to androidx for all files

--- app/src/main/java/com/caren/unobliviate/MainActivity.java
+++ app/src/main/java/com/caren/unobliviate/MainActivity.java
@@ -1,9 +1,10 @@
 package com.caren.unobliviate;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
